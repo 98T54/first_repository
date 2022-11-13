5ba3045f80c845fc36403b73851b8f33ce374d48
98T54 <xaviniesta13@gmail.com>
2022-11-13 17:31:05 +0900

    show_error and store_comments

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -23,7 +24,7 @@ class PostController extends Controller
         return view('posts/show')->with(['post' => $post]);
     }
     
-    public function store(Request $request, Post $post)
+    public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
